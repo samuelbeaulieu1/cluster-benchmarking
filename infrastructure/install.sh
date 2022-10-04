238c4b960fc6b85481d705ef0e880795c83bfc46
Samuel Beaulieu <samuel.b1015@hotmail.com>
2022-10-04 12:03:44 -0400

    Removing stuff to be moved to terraform + change install script to use IAM

--- infrastructure/install.sh
+++ infrastructure/install.sh
@@ -19,13 +19,6 @@ echo \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-# Setup AWS CLI credentials from environment variables
-sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-sudo aws configure set aws_session_token $AWS_SESSION_TOKEN
-sudo aws configure set default.region $REGION
-sudo aws configure set default.output json
-
 sudo aws ecr get-login-password --region ${REGION} | sudo docker login --username AWS --password-stdin ${CONTAINER_REPOSITORY_URL}
 
 # Running app from docker container repository with env vars as input to script
