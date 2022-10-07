1bd9b04d06aee74380bdad58581dd53b31d024fa
Samuel Beaulieu <samuel.b1015@hotmail.com>
2022-10-07 19:11:39 -0400

    Fix race condition for boto3 elb instance in benchmark

--- benchmark/app_test.py
+++ benchmark/app_test.py
@@ -9,10 +9,8 @@ from urllib3.util.retry import Retry
 from datetime import date, timedelta, datetime
 
 
-def call_endpoint_http(cluster:str, count: int = 1):
-    elb = get_client("elbv2")
-    load_balancer = elb.describe_load_balancers(Names=["c1-c2-alb"])["LoadBalancers"][0]
-    url = f"http://{load_balancer['DNSName']}/{cluster}"
+def call_endpoint_http(cluster:str, lb_domain: str, count: int = 1):
+    url = f"http://{lb_domain}/{cluster}"
     headers = {'content-type': "application/json"}
     for i in range(0, count):
         session = requests.Session()
@@ -23,15 +21,15 @@ def call_endpoint_http(cluster:str, count: int = 1):
         print(f"{response.text} <{response.status_code}>")
 
 
-def seq_test(cluster: str):
-    call_endpoint_http(cluster, 1000)
+def seq_test(cluster: str, lb_domain: str):
+    call_endpoint_http(cluster, lb_domain, 1000)
 
-def seq_with_sleep_test(cluster: str):
-    call_endpoint_http(cluster, 500)
+def seq_with_sleep_test(cluster: str, lb_domain: str):
+    call_endpoint_http(cluster, lb_domain, 500)
 
     time.sleep(60)
 
-    call_endpoint_http(cluster, 1000)
+    call_endpoint_http(cluster, lb_domain, 1000)
 
 def get_client(resource_name: str):
     return boto3.client(
@@ -42,9 +40,7 @@ def get_client(resource_name: str):
         aws_session_token=os.environ["AWS_SESSION_TOKEN"]
     )
 
-def retrieve_metrics(start_date, end_date, cluster: str):
-    elb = get_client("elbv2")
-    load_balancer = elb.describe_load_balancers(Names=["c1-c2-alb"])["LoadBalancers"][0]
+def retrieve_metrics(start_date, end_date, cluster: str, load_balancer):
     load_balancer_arn = load_balancer["LoadBalancerArn"].split("loadbalancer/")[-1]
     target_group_arn = elb.describe_target_groups(Names=[cluster])["TargetGroups"][0]["TargetGroupArn"].split(":")[-1]
 
@@ -124,9 +120,12 @@ def retrieve_metrics(start_date, end_date, cluster: str):
 if __name__ == "__main__":
     start = datetime.now()
     threads = []
+    elb = get_client("elbv2")
+    load_balancer = elb.describe_load_balancers(Names=["c1-c2-alb"])["LoadBalancers"][0]
+    lb_domain = load_balancer['DNSName']
     
-    threads.append(threading.Thread(target=seq_test, args=["cluster1"]))
-    threads.append(threading.Thread(target=seq_with_sleep_test, args=["cluster2"]))
+    threads.append(threading.Thread(target=seq_test, args=["cluster1", lb_domain]))
+    threads.append(threading.Thread(target=seq_with_sleep_test, args=["cluster2", lb_domain]))
     
     for thread in threads:
         thread.start()
@@ -139,5 +138,5 @@ if __name__ == "__main__":
     
     end = datetime.now()
     
-    retrieve_metrics(start, end, "cluster1")
-    retrieve_metrics(start, end, "cluster2")
+    retrieve_metrics(start, end, "cluster1", load_balancer)
+    retrieve_metrics(start, end, "cluster2", load_balancer)
